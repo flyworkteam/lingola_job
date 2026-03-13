e4823554ec67ccfb95360496221ce66cc4532a96
Kadir Karataş <kadirkaratass112@gmail.com>
2026-03-13 05:47:03 +0300

    Delete Desktop/adsız klasör 3/lingola_app_backend directory

--- Desktop/adsız klasör 3/lingola_app_backend/Desktop/adsız klasör 3/lingola_app_backend/jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('jest').Config} */
-module.exports = {
-  testEnvironment: "node",
-  testMatch: ["**/__tests__/**/*.test.js"],
-  collectCoverageFrom: ["src/**/*.js", "!src/config/**", "!src/__tests__/**"],
-  coverageDirectory: "coverage",
-  verbose: true,
-  watchman: false,
-};
